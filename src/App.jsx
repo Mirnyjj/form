494152b8a611836aa579ea4b81ffd7dbe79d041c
Maksim <maks1733@gmail.com>
2023-11-12 20:39:33 +0400

    Made an application using React Hook Form and Yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,84 @@
-import { useEffect, useRef, useState } from 'react';
+
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import './App.css'
-import { useStore } from './hooks/useStore';
-import { EmailValidation } from './validation/mailError';
-import { PasswordCheck } from './validation/passwordCheck';
-import { PasswordValidation } from './validation/passwordError';
+import { useEffect, useRef } from 'react';
 
 const sendFormData = (formData) => {
   console.log(formData);
-}
+};
 
-function App() {
-  const { getState, updateState } = useStore();
-  const [emailValid, setEmailValid] = useState(false);
-  const [passwordValid, setPasswordValid] = useState(false);
+const fieldsSchema = yup.object()
+  .shape({
+    email: yup.string()
+      .matches(/^[\w]+@[\w-]+\.[a-z]{2,4}$/, 'Невырый email. Допустимый формат почты: example@exam.ru')
+      .max(20, 'Невырый email. Должно быть не больше 20 символов'),
+    password: yup.string()
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, 'Неверный пароль. Пароль должен содержать цифры и латинские буквы верхнего и нижнего регистра без пробела')
+      .max(10, 'Неверный пароль. Пароль должен быть не больше 10 символов')
+      .min(6, 'Неверный пароль. Пароль должен быть не меньше 6 символов'),
+    checkPassword: yup.string()
+      .oneOf([yup.ref('password'), null], 'Введенные пароли не совпадают')
+  })
+
+export const App = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      checkPassword: '',
+    },
+    resolver: yupResolver(fieldsSchema),
+  });
+
+  const emailError = errors.email?.message;
+  const passwordError = errors.password?.message;
+  const passwordCheckError = errors.checkPassword?.message;
+
   const submitButtonRef = useRef(null);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    sendFormData(getState());
-  }
 
-
-  const { email, checkPassword, password } = getState();
-  const onChange = ({ target }) => {
-    updateState(target.name, target.value);
-  }
-  console.log(emailValid, passwordValid)
   useEffect(() => {
-    if (emailValid || passwordValid || password === checkPassword) {
+    if (!emailError && !passwordError && !passwordCheckError) {
       submitButtonRef.current.focus();
-      console.log(emailValid, passwordValid)
-    } [emailValid, passwordValid, checkPassword]
+      console.log(emailError, passwordError, passwordCheckError)
+    } [emailError, passwordError, passwordCheckError]
   });
 
   return (
     <div className='header'>
-      <form onSubmit={onSubmit}>
-        <div className='error'>
-          <div>
-            <EmailValidation
-              email={email}
-              setEmailValid={setEmailValid} />
-          </div>
-          <div>
-            <PasswordValidation
-              password={password}
-              setPasswordValid={setPasswordValid} />
-          </div>
-          <div>
-            <PasswordCheck
-              password={password}
-              checkPassword={checkPassword} />
-          </div>
-        </div>
+      <form onSubmit={handleSubmit(sendFormData)}>
+        {emailError && <div className='error'>{emailError}</div>}
+        {passwordError && <div className='error'>{passwordError}</div>}
+        {passwordCheckError && <div className='error'>{passwordCheckError}</div>}
         <input
-          name='email'
-          type='email'
+          name="email"
           placeholder='Введите почту'
-          value={email}
-          onChange={onChange}
+          type="text"
+          {...register('email')}
         />
         <input
           name='password'
           type='password'
           placeholder='Введите пароль'
-          value={password}
-          onChange={onChange}
+          {...register('password')}
         />
         <input
           name='checkPassword'
           type='password'
           placeholder='Повторите пароль'
-          value={checkPassword}
-          onChange={onChange}
+          {...register('checkPassword')}
         />
-        <button
-          type='submit'
-          disabled={!emailValid || !passwordValid || password !== checkPassword}
-          ref={submitButtonRef}>
-          Зарегистрироваться
-        </button>
+        <button ref={submitButtonRef} type="submit" disabled={!!emailError || !!passwordError}>Отправить</button>
       </form>
-
     </div>
-  )
-}
+  );
+};
+
 
 export default App
